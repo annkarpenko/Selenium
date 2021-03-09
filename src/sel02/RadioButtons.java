91280d9f6edae11aedb10668c0056b2c71f3de49
annkarpenko <volion.dnepr@gmail.com>
2021-03-09 13:30:31 +0200

    selenium - radio buttons

---
+++ src/sel02/RadioButtons.java
@@ -0,0 +1,50 @@
+package sel02;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.List;
+
+public class RadioButtons {
+    public static void main(String[] args) {
+        String url = "http://www.leafground.com/pages/radio.html";
+        ChromeDriver driver = new ChromeDriver();
+
+        //open a window
+        driver.get(url);
+
+        //1. Choose Yes by Id
+        driver.findElementByCssSelector("#yes").click();
+
+        //2.Find default selected radio button
+        //Create list of inputs by name
+        List<WebElement> input = driver.findElements(By.name("news"));
+
+        //find input values
+        for (WebElement webElement : input) {
+            String checked = webElement.getAttribute("checked");
+            if (checked == null) {
+                checked = "false";
+            }
+
+            String labelText = ((RemoteWebElement) input.get(1)).findElementByXPath("..").getText();
+            if (checked.equals("true")) {
+                System.out.println("The title of default radio button: " + labelText);
+            }
+        }
+
+        //Select age group
+        int myAge = 18;
+
+        if (myAge < 21) {
+            driver.findElementByXPath("//*[@id=\"contentblock\"]/section/div[3]/div/div/input[1]").click();
+        } else if (myAge > 40){
+            driver.findElementByXPath("//*[@id=\"contentblock\"]/section/div[3]/div/div/input[3]").click();
+        } else {
+            driver.findElementByXPath("//*[@id=\"contentblock\"]/section/div[3]/div/div/input[2]").click();
+        }
+
+    }
+}
